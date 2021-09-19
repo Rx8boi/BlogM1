0c273066baa97993287b83d49f0d8178f4b37738
Charles Butler <cdbutle@outlook.com>
2021-09-19 09:58:20 -0500

    get request for api

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+
+  var $posts = $('#posts');
+
+  // $.ajax({
+  //   type: "GET",
+  //   url: "https://jsonplaceholder.typicode.com/posts",
+  //   success: function (posts) {
+  //     // console.log('success', posts);
+  //     $.each(posts, function (i, post) {
+  //       $posts.append('<li><h4>'+ post.title +'</h4></br><p>'+ post.body +'</p></li>')
+  //     })
+  //   }
+  // });
+
+  $.ajax({
+    type: "GET",
+    url: "https://jsonplaceholder.typicode.com/posts?userId=1",
+    success: function (posts) {
+      // console.log('success', posts);
+      $.each(posts, function (i, post) {
+        $posts.append(
+          "<li id='entry'><a href='#' target='_blank'><h4>" + post.title + "</h4></a></br><p>" + post.body.substring(0, 15)+'...' + "</p></li>"
+        );
+      });
+    },
+  });
+
+
+
+});
